7d54d2f4530743d38226c6c65cb6a5129fadcf32
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-05-25 21:54:51 +0000

    about section created

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
 
 
      {/* Home section */}
-     
+
      <section class="home" id="home">
         <div class="max-width">
           <div class="home-content">
@@ -55,6 +55,33 @@ function App() {
               And I'm a <span class="typing"></span>
             </div>
             <a href="#">Hire me</a>
+          </div>
+        </div>
+      </section>
+
+
+      {/* About section */}
+
+      <section class="about" id="about">
+        <div class="max-width">
+          <h2 class="title">About me</h2>
+          <div class="about-content">
+            <div class="column right">
+              <div class="text">
+                I'm Prakash and I'm a <span class="typing-2"></span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                ut voluptatum eveniet doloremque autem excepturi eaque, sit
+                laboriosam voluptatem nisi delectus. Facere explicabo hic minus
+                accusamus alias fuga nihil dolorum quae. Explicabo illo unde,
+                odio consequatur ipsam possimus veritatis, placeat, ab molestiae
+                velit inventore exercitationem consequuntur blanditiis omnis
+                beatae. Dolor iste excepturi ratione soluta quas culpa
+                voluptatum repudiandae harum non.
+              </p>
+              <a href="#">Download CV</a>
+            </div>
           </div>
         </div>
       </section>
