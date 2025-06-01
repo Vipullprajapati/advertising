e5c963b2711482b7776682340a781cfc11097249
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-06-01 20:16:16 +0000

    add login page from univers.io

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
 import "./App.css";
-
-
-
-
-
-
 
 function App() {
   return (
-     
-
     <>
-
-
-
       <div class="scroll-up-btn">
         <i class="fas fa-angle-up"></i>
       </div>
@@ -255,6 +243,38 @@ function App() {
       </section>
 
       {/* login section */}
+
+      <div class="container" >
+        <input id="signup_toggle" type="checkbox" />
+        <form class="form">
+          <div class="form_front">
+            <div class="form_details">Login</div>
+            <input type="text" class="input" placeholder="Username" />
+            <input type="text" class="input" placeholder="Password" />
+            <button class="btn">Login</button>
+            <span class="switch">
+              Don't have an account?
+              <label for="signup_toggle" class="signup_tog">
+                Sign Up
+              </label>
+            </span>
+          </div>
+          <div class="form_back">
+            <div class="form_details">SignUp</div>
+            <input type="text" class="input" placeholder="Firstname" />
+            <input type="text" class="input" placeholder="Username" />
+            <input type="text" class="input" placeholder="Password" />
+            <input type="text" class="input" placeholder="Confirm Password" />
+            <button class="btn">Signup</button>
+            <span class="switch">
+              Already have an account?
+              <label for="signup_toggle" class="signup_tog">
+                Sign In
+              </label>
+            </span>
+          </div>
+        </form>
+      </div>
 
       {/* Footer section */}
 
